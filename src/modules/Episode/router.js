3329d5fd63f5dbdd7173d445051a4150c4607ceb
davy chen <davych@163.com>
2021-09-14 21:07:48 +0800

    fix detail

--- src/modules/Episode/router.js
+++ src/modules/Episode/router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Detail from './Detail';
+import Detail from './detail';
 
 export default function Eposide() {
   return (
